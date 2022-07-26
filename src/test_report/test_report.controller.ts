aab8644b860ca5ef1f80aeb376e55cc6e16e9e1a
Yashwanth Kumar Pamidimukkala <ykp3@gatech.edu>
2022-07-26 18:42:00 -0400

    added query params to get call

--- src/test_report/test_report.controller.ts
+++ src/test_report/test_report.controller.ts
@@ -3,10 +3,10 @@ import { TestReportService } from './test_report.service';
 
 @Controller('test-report')
 export class TestReportController {
-    constructor(private readonly testReportService : TestReportService) {}
+    constructor(private readonly testReportService: TestReportService) {}
 
     @Get(":student_id/:test_name")
-    async provideReport(@Param(":student") student_id : number, @Param(":exam") test_name : string) : Promise<any> {
+    async provideReport(@Param(":student") student_id: number, @Param(":exam") test_name: string): Promise<any> {
         return this.testReportService.provideReport(student_id, test_name)
     }
 }
